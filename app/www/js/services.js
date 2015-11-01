05609ab134fb2c76c2e8c1cdaeac894969c51269
ivanalbizu <ivan.albizu@gmail.com>
2015-11-01 18:14:21 +0100

    Delete a Service not used

--- app/www/js/services.js
+++ app/www/js/services.js
@@ -36,42 +36,6 @@ angular.module('starter.services', [])
   }
 })
 
-.factory(
-  "transformRequestAsFormPost",
-  function() {
-      function transformRequest( data, getHeaders ) {
-          var headers = getHeaders();
-          headers[ "Content-type" ] = "application/x-www-form-urlencoded; charset=utf-8";
-          return( serializeData( data ) );
-      }
-      return( transformRequest );
-
-      function serializeData( data ) {
-          if ( ! angular.isObject( data ) ) {
-              return( ( data == null ) ? "" : data.toString() );
-          }
-          var buffer = [];
-          for ( var name in data ) {
-              if ( ! data.hasOwnProperty( name ) ) {
-                  continue;
-              }
-              var value = data[ name ];
-              buffer.push(
-                  encodeURIComponent( name ) +
-                  "=" +
-                  encodeURIComponent( ( value == null ) ? "" : value )
-              );
-          }
-          var source = buffer
-              .join( "&" )
-              .replace( /%20/g, "+" )
-          ;
-          return( source );
-      }
-  })
-
-
-
 .factory('Invoices', function($http, $q) {
   return {
     get: function() {
